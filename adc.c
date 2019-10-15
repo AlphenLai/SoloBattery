bc6b82a7ce914b6bc74edbc2b6c89ea3c0415395
Alvin <alvin.lai@hex.aero>
2019-10-15 20:30:45 +0800

    add ADC header files

---
+++ adc.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+#include "ch.h"
+#include "hal.h"
+
+#include "adc.h"
+
+#ifdef HAL_USE_ADC
+
+#endif
---
+++ adc.h
@@ -0,0 +1,12 @@
+#ifndef adc_H_
+#define adc_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* adc_H_ */
