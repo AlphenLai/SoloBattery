278c458d56f15755cdb9beceba1ad288c76834f1
Alvin <alvin.lai@hex.aero>
2019-10-15 20:34:36 +0800

    Change Charge EN; disabled ADC temporarily

--- main.c
+++ main.c
@@ -20,6 +20,7 @@
 #include "bq76920_driver.h"
 #include "i2c_slave_interface.h"
 #include "LED.h"
+#include "adc.h"
 
 #include <math.h>
 
@@ -32,24 +33,27 @@ uint16_t TS1;
 uint16_t RT2_FLT;
 float cellsVolt[5];
 double battery_percentage;
+bool charging_status;
+#define discharging         0
+#define charging            1
 
-static adcsample_t RT2_adc[ADC1_NUM_CHANNELS * ADC1_BUF_DEPTH];  //12bit
+// static adcsample_t RT2_adc[ADC1_NUM_CHANNELS * ADC1_BUF_DEPTH];  //12bit
 
 /*
  * ADC streaming callback.
  */
  
-static void adccallback(ADCDriver *adcp) {
+// static void adccallback(ADCDriver *adcp) {
 
-  if (adcIsBufferComplete(adcp)) {
-  }    
-}
+//   if (adcIsBufferComplete(adcp)) {
+//   }    
+// }
 
-static void adcerrorcallback(ADCDriver *adcp, adcerror_t err) {
+// static void adcerrorcallback(ADCDriver *adcp, adcerror_t err) {
 
-  (void)adcp;
-  (void)err;
-}
+//   (void)adcp;
+//   (void)err;
+// }
 
 /*
  * input clock = 24MHz.
@@ -67,16 +71,16 @@ static const I2CConfig i2cfg1 = {
  * Mode:        Linear buffer, 8 samples of 1 channel, SW triggered.
  * Channels:    IN1.
  */
-static const ADCConversionGroup adccfg1 = {
- FALSE,
- ADC1_NUM_CHANNELS,
- NULL,
- NULL,
- ADC_CFGR1_CONT | ADC_CFGR1_RES_12BIT,             /* CFGR1 */
- ADC_TR(0, 0),                                     /* TR */
- ADC_SMPR_SMP_1P5,                                 /* SMPR */
- ADC_CHSELR_CHSEL1                                /* CHSELR */
-};
+// static const ADCConversionGroup adccfg1 = {
+//  FALSE,
+//  ADC1_NUM_CHANNELS,
+//  NULL,
+//  NULL,
+//  ADC_CFGR1_CONT | ADC_CFGR1_RES_12BIT,             /* CFGR1 */
+//  ADC_TR(0, 0),                                     /* TR */
+//  ADC_SMPR_SMP_1P5,                                 /* SMPR */
+//  ADC_CHSELR_CHSEL1                                /* CHSELR */
+// };
 
 /*
  * LEDs batLED thread, times are in milliseconds.
@@ -87,10 +91,18 @@ static THD_FUNCTION(Thread1, arg) {
   (void)arg;
   chRegSetThreadName("batLED");
   while (true) {
-    chThdSleepMilliseconds(500);
-    palSetPad(GPIOB, GPIOB_LED5);
-    chThdSleepMilliseconds(500);
-    palClearPad(GPIOB, GPIOB_LED5);
+    if(charging_status == discharging) {
+      chThdSleepMilliseconds(500);
+      palSetPad(GPIOB, GPIOB_LED5);
+      chThdSleepMilliseconds(500);
+      palClearPad(GPIOB, GPIOB_LED5);
+    }
+    else {
+      chThdSleepMilliseconds(100);
+      palSetPad(GPIOB, GPIOB_LED5);
+      chThdSleepMilliseconds(100);
+      palClearPad(GPIOB, GPIOB_LED5);
+    }
   }
 }
 
@@ -123,8 +135,8 @@ int main(void) {
   /*
    * Activates the ADC1 driver and the temperature sensor.
    */
-   adcStart(&ADCD1, NULL);
-   adcSTM32SetCCR(ADC_CCR_TSEN | ADC_CCR_VREFEN);
+  //  adcStart(&ADCD1, NULL);
+  //  adcSTM32SetCCR(ADC_CCR_TSEN | ADC_CCR_VREFEN);
 
   chThdSleepMilliseconds(1000);
 
@@ -135,20 +147,25 @@ int main(void) {
   battery_init(battery_mAh);
   chThdSleepMilliseconds(100);
 
+  // DischargeEN();
+  // charging_status = discharging;
+  ChangeBatteryStatus(Charge);
+  charging_status = charging;
+
   /*
    * Normal main() thread activity, in this demo it does nothing.
    */
   while (true) {
     //msg_t msg;
     //i2cflags_t err;
-    DischargeEN();
     
     //the hotter temperature, the smaller the value
     I2CReadRegisterWordWithCRC(&I2CD1, addr_bq76920, TS1_HI, &TS1);
-    adcStartConversion(&ADCD1, &adccfg1, RT2_adc, ADC1_BUF_DEPTH);
-    RT2_FLT = RT2_adc[0];
+    // adcStartConversion(&ADCD1, &adccfg1, RT2_adc, ADC1_BUF_DEPTH);
+    // RT2_FLT = RT2_adc[0];
 
     GetCellsVolt(cellsVolt);
+    chThdSleepMilliseconds(100);
 
     if (palReadPad(GPIOA, GPIOA_ALERT))
     {
@@ -164,7 +181,6 @@ int main(void) {
     // Set_LED(0);
     // chThdSleepMilliseconds(500);
 
-    chThdSleepMilliseconds(500);
     if (battery_percentage >= 99) {
       //100-99.1
       palClearPad(GPIOA, GPIOA_LED1);
@@ -195,12 +211,18 @@ int main(void) {
       palSetPad(GPIOA, GPIOA_LED2);
       palSetPad(GPIOA, GPIOA_LED3);
       palSetPad(GPIOB, GPIOB_LED4);
-      ChargeEN();
+      ChangeBatteryStatus(Charge);
     }
-    chThdSleepMilliseconds(500);
     
-    // if (palReadPad(GPIOA, GPIOA_WKUP1)) {
-    //   //do something when the button is being pressed
-    //}
+    if (palReadPad(GPIOA, GPIOA_WKUP1)) {
+      //do something when the button is being pressed
+      if(charging_status == discharging) {
+        ChangeBatteryStatus(Charge);
+        charging_status = charging;
+      } else {
+        ChangeBatteryStatus(Discharge);
+        charging_status = discharging;
+      }
+    }
   }
 }
